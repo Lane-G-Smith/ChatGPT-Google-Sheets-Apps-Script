7762e3456880e674dc2c060ba503582a9776853c
Lane G Smith <108886772+Lane-G-Smith@users.noreply.github.com>
2023-03-08 15:26:20 -0700

    Submit script for gpt-3.5-turbo api call

---
+++ gpt-3.5-turbo.js
@@ -0,0 +1,31 @@
+const SECRET_KEY = "";
+const MAX_TOKENS = 500;
+   
+    function TURBO(prompt, model="gpt-3.5-turbo", temperature=0.5) {
+     let apiKey = SECRET_KEY;
+     let endpoint = "https://api.openai.com/v1/chat/completions";
+     let payload = {
+       messages:[
+        {role: 'system', content: 'This is where you describe what cpt does and how it answers.'},
+        {role: 'user', content: 'This is an example question'},
+        {role: 'assistant', content: "This is an example answer"},
+        {role: 'user', content: `${prompt}`},
+            ],
+       model: model,
+       temperature: temperature,
+       max_tokens: MAX_TOKENS,
+     };
+     let options = {
+       method: "post",
+       contentType: "application/json",
+       payload: JSON.stringify(payload),
+       headers: {
+         "Authorization": "Bearer " + apiKey
+       }
+     };
+     
+     let response = UrlFetchApp.fetch(endpoint, options);
+     let json = response.getContentText();
+     let data = JSON.parse(json);    
+     return data.choices[0].message.content.trim();
+   }
